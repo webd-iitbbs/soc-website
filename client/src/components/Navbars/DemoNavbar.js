fe3772e0ac860eea3412850c4529f024d4a17b5d
Sudheer Bulusu <bds15@iitbbs.ac.in>
2021-01-29 11:32:11 +0530

    sixth commit

--- client/src/components/Navbars/DemoNavbar.js
+++ client/src/components/Navbars/DemoNavbar.js
@@ -181,7 +181,7 @@ class Header extends React.Component {
                 <DropdownToggle caret nav>
              
                   <p>
-                  <img src={this.state.user.photo}/> {this.state.user.name}
+                  <img src={this.state.user.photo}/> this.state.name ? {this.state.name} : Guest
                   </p>
                 </DropdownToggle>
                 <DropdownMenu right>
