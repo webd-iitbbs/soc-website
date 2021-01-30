fe150128bc9de5cffa923ce4780cb6d03cf150a1
Sudheer Bulusu <bds15@iitbbs.ac.in>
2021-01-30 21:46:34 +0530

    msg

--- client/src/views/Dashboard.js
+++ client/src/views/Dashboard.js
@@ -16,6 +16,35 @@ import {
 
 
 class Dashboard extends React.Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      user:''
+      };
+  }
+
+  componentDidMount () {
+    console.log('hi')
+    axios.get('/user')
+    .then(response => {
+        console.log('hello')
+        this.setState({ user: response.data });
+    })
+  }
+
+  loginState(){
+    return (
+      this.state.user.name ?  <>
+      <p className="card-category">Login with your Institute mailId (if you haven't, if you have logged in you won't be seeing this, lol :)</p>
+      <p className="card-category">Login with the same email, so that we an track your progress..</p>
+      <a href="/auth/google"><button type="button" class="btn btn-outline-info" >LOGIN</button></a>
+      </>: <>
+        
+      </>
+    );
+  }
+
   render() {
     return (
       <>
@@ -35,9 +64,7 @@ some of the important concepts of JavaScript. </p>
                   <p className="card-category">To make sure you have gained knowledge out of it, we will be assessing you on the weekends. </p>
                   <p className="card-category">Proceed to dashboard and find links for Week1 and Week2</p>
                   <p className="card-category">Proceed to week 2 only if you have completed week 1</p>
-                  <p className="card-category">Login with your Institute mailId (if you haven't, if you have logged in your name should be appearing on top, lol :)</p>
-                  <p className="card-category">Login with the same email, so that we an track your progress..</p>
-                  <a href="/auth/google"><button type="button" class="btn btn-outline-info" >LOGIN</button></a>
+                  {this.loginState()}
                 </CardHeader>
                
                 <CardFooter>
