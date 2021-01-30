cfa31f9692e847e9a9692ed2cd57527b227ab9e1
Sudheer Bulusu <bds15@iitbbs.ac.in>
2021-01-30 17:41:41 +0530

    check

--- client/src/views/Dashboard.js
+++ client/src/views/Dashboard.js
@@ -25,14 +25,26 @@ class Dashboard extends React.Component {
             <Col md="12">
               <Card>
                 <CardHeader>
-                  <CardTitle tag="h5">WELCOME TO OUR WORKSHOP</CardTitle>
+                  <CardTitle tag="h5">WELCOME TO THE WORKSHOP</CardTitle>
                   <p className="card-category">THIS IS A 2 WEEK WORKSHOP</p>
-                  <p className="card-category">After every week you will have an assessment to check your understanding</p>
-                  <p className="card-category">After this workshop, you will get a basic idea about web devolepment</p>
+                  <p className="card-category">In which , you will be learning 
+the important concepts of HTML, 
+styling the web pages using CSS, 
+adding responsiveness to the web pages using CSS, 
+some of the important concepts of JavaScript. </p>
+                  <p className="card-category">To make sure you have gained knowledge out of it, we will be assessing you on the weekends. </p>
                   <p className="card-category">Proceed to dashboard and find links for Week1 and Week2</p>
-                  <p className="card-category">Login with your Institute mailId</p>
+                  <p className="card-category">Proceed to week 2 only if you have completed week 1</p>
+                  <p className="card-category">Login with your Institute mailId (if you haven't, if you have logged in your name should be appearing on top, lol :)</p>
+                  <p className="card-category">Login with the same email, so that we an track your progress..</p>
                   <a href="/auth/google"><button type="button" class="btn btn-outline-info" >LOGIN</button></a>
                 </CardHeader>
+ 
+
+To make sure you have gained knowledge out of it, we will be assessing you on the weekends. 
+
+What are you waiting for ? 
+Grab the chance, and head to webd-iitbbs.herokuapp.com to register for the workshop.
                
                 <CardFooter>
                   <hr />
