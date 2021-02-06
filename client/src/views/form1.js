97c52a358a83df64f53aaed3a37601e18c413e2a
Sudheer Bulusu <bds15@iitbbs.ac.in>
2021-02-06 11:17:23 +0530

    added assignment1

--- client/src/views/form1.js
+++ client/src/views/form1.js
@@ -113,105 +113,105 @@ class Form1 extends React.Component {
                 <form onSubmit={this.handleSubmit}>
             <div className="form-group">
               <label for="qone">Question 1</label>
-              <h5>A frontend cum backend developer using a common platform across the entire stack is known as</h5>
+              <h6>A frontend cum backend developer using a common platform across the entire stack is known as</h6>
               <input type="text" name="qone" value={this.state.qone} onChange={this.handleChange} className="form-control" id="qone"  />
             </div>
             <div className="form-group">
               <label for="qone">Question 2</label>
-              <h5>What is the difference between HTTP and HTTPs?</h5>
+              <h6>What is the difference between HTTP and HTTPs?</h6>
               <input type="text" name="qtwo" value={this.state.qtwo} onChange={this.handleChange} className="form-control" id="qone"  />
             </div>
             <div className="form-group">
               <label for="qone">Question 3</label>
-              <h5>HTML,CSS,Bootstrap are part of which component of a website:</h5>
+              <h6>HTML,CSS,Bootstrap are part of which component of a website:</h6>
               <input type="text" name="qthree" value={this.state.three} onChange={this.handleChange} className="form-control" id="qone"  />
             </div>
             <div className="form-group">
               <label for="qone">Question 4</label>
-              <h5>Websites that can adjust according to screen size are called:</h5>
+              <h6>Websites that can adjust according to screen size are called:</h6>
               <input type="text" name="qfour" value={this.state.qfour} onChange={this.handleChange} className="form-control" id="qone"  />
             </div>
             <div className="form-group">
               <label for="qone">Question 5</label>
-              <h5>How is Github different from Git</h5>
+              <h6>How is Github different from Git</h6>
               <input type="text" name="qfive" value={this.state.qfive} onChange={this.handleChange} className="form-control" id="qone"  />
             </div>
             <div className="form-group">
               <label for="qone">Question 6</label>
-              <h5>The main branch that hosts packages and other data in Github is known as:</h5>
+              <h6>The main branch that hosts packages and other data in Github is known as:</h6>
               <input type="text" name="qsix" value={this.state.qsix} onChange={this.handleChange} className="form-control" id="qone"  />
             </div>
             <div className="form-group">
               <label for="qone">Question 7</label>
-              <h5>The process of updating anything in a Github Repo is known as:</h5>
+              <h6>The process of updating anything in a Github Repo is known as:</h6>
               <input type="text" name="qsix" value={this.state.qseven} onChange={this.handleChange} className="form-control" id="qone"  />
             </div>
             <div className="form-group">
               <label for="qone">Question 8</label>
-              <h5>The command that is used to update a local file system to a git repo is:</h5>
+              <h6>The command that is used to update a local file system to a git repo is:</h6>
               <input type="text" name="qsix" value={this.state.qeight} onChange={this.handleChange} className="form-control" id="qone"  />
             </div>
             <div className="form-group">
               <label for="qone">Question 9</label>
-              <h5>The command that is used to update a local file system to a git repo is:</h5>
+              <h6>The command that is used to update a local file system to a git repo is:</h6>
               <input type="text" name="qsix" value={this.state.qnine} onChange={this.handleChange} className="form-control" id="qone"  />
             </div>
             <div className="form-group">
               <label for="qone">Question 10</label>
-              <h5>Which tag is used to give a line break ?</h5>
+              <h6>Which tag is used to give a line break ?</h6>
               <input type="text" name="qsix" value={this.state.qten} onChange={this.handleChange} className="form-control" id="qone"  />
             </div>
             <div className="form-group">
               <label for="qone">Question 11</label>
-              <h5>What is the font-size of the h1 heading tag?</h5>
+              <h6>What is the font-size of the h1 heading tag?</h6>
               <input type="text" name="qsix" value={this.state.qeleven} onChange={this.handleChange} className="form-control" id="qone"  />
             </div>
 
             <div className="form-group">
               <label for="qone">Question 12</label>
-              <h5>Which attribute is used to add link to any element</h5>
+              <h6>Which attribute is used to add link to any element</h6>
               <input type="text" name="qsix" value={this.state.qtwelve} onChange={this.handleChange} className="form-control" id="qone"  />
             </div>
 
             <div className="form-group">
               <label for="qone">Question 13</label>
-              <h5>What is the purpose of using div tags in HTML?</h5>
+              <h6>What is the purpose of using div tags in HTML?</h6>
               <input type="text" name="qsix" value={this.state.qthirteen} onChange={this.handleChange} className="form-control" id="qone"  />
             </div>
             
             <div className="form-group">
               <label for="qone">Question 14</label>
-              <h5>Which of the following tags adds text formatting (without any additional styles - CSS)? (i,b,span,p)</h5>
+              <h6>Which of the following tags adds text formatting (without any additional styles - CSS)? (i,b,span,p)</h6>
               <input type="text" name="qsix" value={this.state.qfourteen} onChange={this.handleChange} className="form-control" id="qone"  />
             </div>
             <div className="form-group">
               <label for="qone">Question 15</label>
-              <h5>The correct way to add background color in HTML?</h5>
+              <h6>The correct way to add background color in HTML?</h6>
               <input type="text" name="qsix" value={this.state.qfifteen} onChange={this.handleChange} className="form-control" id="qone"  />
             </div>
             <div className="form-group">
               <label for="qone">Question 16</label>
-              <h5>What is the difference between Inline , Internal and External CSS?</h5>
+              <h6>What is the difference between Inline , Internal and External CSS?</h6>
               <input type="text" name="qsix" value={this.state.qsixteen} onChange={this.handleChange} className="form-control" id="qone"  />
             </div>
             <div className="form-group">
               <label for="qone">Question 17</label>
-              <h5>Color in CSS can be specified by 3 ways. Write an example of each.</h5>
+              <h6>Color in CSS can be specified by 3 ways. Write an example of each.</h6>
               <input type="text" name="qsix" value={this.state.qseventeen} onChange={this.handleChange} className="form-control" id="qone"  />
             </div>
             <div className="form-group">
               <label for="qone">Question 18</label>
-              <h5>What is the difference between b tag and strong tag?</h5>
+              <h6>What is the difference between b tag and strong tag?</h6>
               <input type="text" name="qsix" value={this.state.qeighteen} onChange={this.handleChange} className="form-control" id="qone"  />
             </div>
             <div className="form-group">
               <label for="qone">Question 19</label>
-              <h5>What is the difference between padding and margin in CSS?</h5>
+              <h6>What is the difference between padding and margin in CSS?</h6>
               <input type="text" name="qsix" value={this.state.qnineteen} onChange={this.handleChange} className="form-control" id="qone"  />
             </div>
             <div className="form-group">
               <label for="qone">Question 20</label>
-              <h5>Write a statement (CSS code) for red colored, dashed border of 8px thickness.</h5>
+              <h6>Write a statement (CSS code) for red colored, dashed border of 8px thickness.</h6>
               <input type="text" name="qsix" value={this.state.qtwenty} onChange={this.handleChange} className="form-control" id="qone"  />
             </div>
 
